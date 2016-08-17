6b3540842be16fdb5e1b45fb366d7757e29b3b6a
Diana Potapenkova <diana.potapenkova@gmail.com>
2016-08-17 11:46:43 +0300

    New test DianaMagnetictest

--- src/test/java/lv/ctco/ts/smoke/magentic_games/DianaMagneticTest.java
+++ src/test/java/lv/ctco/ts/smoke/magentic_games/DianaMagneticTest.java
@@ -1,6 +1,6 @@
 package lv.ctco.ts.smoke.magentic_games;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -22,7 +22,7 @@ public class DianaMagneticTest {
     WebElement siteMapLink = driver.findElement(By.xpath("//div[@class='column information']/ul/li[1]/a"));
     siteMapLink.click();
     WebElement lastCategoryElement = driver.findElement(By.xpath("//div[./h2[text()='Categories']]/following-sibling::div[@class='entity-body']/ul/li[last()]/a"));
-    Assert.assertEquals("Gift Cards",lastCategoryElement.getText());
+    Assert.assertEquals("Gift Cards", lastCategoryElement.getText());
 
 
 
